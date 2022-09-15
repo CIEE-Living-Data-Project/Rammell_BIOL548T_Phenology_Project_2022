8db9928939e08710624243e225c2e541747cf1ad
nicolarammell <nicolarammell@gmail.com>
2022-09-15 16:14:52 -0700

    Updated script to create data folders and download data to the url one

--- scripts/phenology_script.R
+++ scripts/phenology_script.R
@@ -6,3 +6,19 @@ dir.create("data/url")
 dir.create("data/dryad")
 dir.create("data/github")
 
+## download from a url
+# downloaded on 2022-09-15
+data.url <- "https://ftp.maps.canada.ca/pub//nrcan_rncan/Forests_Foret/TLW/TLW_invertebrateDensity.csv"
+metadata.url <- "https://ftp.maps.canada.ca/pub//nrcan_rncan/Forests_Foret/TLW/TLW_invertebrate_metaEN.csv"
+
+getwd()
+data.dest.file <- "/Users/N/Documents/GitHub/BIOL548T/Rammell_BIOL548T_Phenology_Project_2022/data/url/NRCAN_1995_2009_TLW_invert_density.csv"
+metadata.dest.file <- "/Users/N/Documents/GitHub/BIOL548T/Rammell_BIOL548T_Phenology_Project_2022/data/url/NRCAN_1995_2009_TLW_invert_metadata.csv"
+
+download.file(url = data.url, destfile = data.dest.file)
+download.file(url = metadata.url, destfile = metadata.dest.file)
+
+invert.density <- read.csv("data/url/NRCAN_1995_2009_TLW_invert_density.csv")
+View(invert.density)
+
+
